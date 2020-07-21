89238fd76e9ae6f37510eb30b074ae985e214c0b
YRM <yinruiming1219@163.com>
2020-07-21 17:10:52 +0800

    1、首次提交

---
+++ src/main/java/com/springboot/hirui/HiruiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hirui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiruiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiruiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/hirui/HiruiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.hirui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiruiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hi");
+	}
+
+}
